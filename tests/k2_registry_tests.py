f65e1e9006dea52d2b339ff1b9d7d2b4e15d83f4
simon <simon.emmott@yahoo.co.uk>
2020-04-13 13:51:34 +0100

    Added test setup

---
+++ tests/k2_registry_tests.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+class K2RegistryTests(TestCase):
+    
+    def test_test(self):
+        self.assertTrue(True, 'Failed to test')
+        
+    # TODO: It should include a registrations list with initial value []
+    
+    # TODO: It should include a registration decorator method which adds registrations to the list of global registrations
+    
+    # TODO: It should define the Registry class
+    
+    # TODO: It should include a Registry instance named registry
+    
+    # TODO: It should include a register decorator method which registers an item with the global Registry
+    
+    # TODO: It should include a retrieve method which retrieves items from the global registry
+    
+        
+        
+if __name__ == '__main__':
+    unittest.main()
